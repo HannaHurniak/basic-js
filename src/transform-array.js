6fee9eb17b192c9c595b63a43b4f5ebbc29314dc
HannaHurniak <hanna.hurniak@yahoo.com>
2021-03-28 22:33:51 +0300

    feat: add some task

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,22 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  // throw new CustomError('Not implemented');
+  let array = arr.slice();
+
+ for (let i = 0; i < array.length; i++) {
+   if (array[i] == '--discard-next') {
+    array.splice((i), 2)
+   }
+   if (array[i] == '--discard-prev') {
+    array.splice((i - 1), 2)
+   }
+   if (array[i] == '--double-next') {
+    array[i] = array[i + 1]
+   }
+   if (array[i] == '--double-prev') {
+    array[i] = array[i - 1]
+   }
+  }
+ return array;
 };
--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,12 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  // throw new CustomError('Not implemented');
+  if (date == null) return 'Unable to determine the time of year!';
+  if (!(date instanceof Date)) throw new Error();
+  if (!date) return Error;
+  let month = date.getMonth();
+  let timeOfTheYear = ['winter', 'winter', 'spring', 'spring', 'spring', 'summer', 'summer', 'summer', 'autumn', 'autumn', 'autumn', 'winter'];
+  
+  return timeOfTheYear[month]; 
 };
