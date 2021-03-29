2cf6da33ba1c92add303c963e188123ffc5a6565
HannaHurniak <hanna.hurniak@yahoo.com>
2021-03-29 21:48:49 +0300

    feat: add check, function

--- src/transform-array.js
+++ src/transform-array.js
@@ -2,21 +2,56 @@ const CustomError = require("../extensions/custom-error");
 
 module.exports = function transform(arr) {
   // throw new CustomError('Not implemented');
-  let array = arr.slice();
+//   let array = arr.slice();
 
- for (let i = 0; i < array.length; i++) {
-   if (array[i] == '--discard-next') {
-    array.splice((i), 2)
-   }
-   if (array[i] == '--discard-prev') {
-    array.splice((i - 1), 2)
-   }
-   if (array[i] == '--double-next') {
-    array[i] = array[i + 1]
-   }
-   if (array[i] == '--double-prev') {
-    array[i] = array[i - 1]
-   }
+//  for (let i = 0; i < array.length; i++) {
+//    if (array[i] == '--discard-next') {
+//     array.splice((i), 2)
+//    }
+//    if (array[i] == '--discard-prev') {
+//     array.splice((i - 1), 2)
+//    }
+//    if (array[i] == '--double-next') {
+//     array[i] = array[i + 1]
+//    }
+//    if (array[i] == '--double-prev') {
+//     array[i] = array[i - 1]
+//    }
+//   }
+//  return array;
+// };
+
+try {
+  if(Object.prototype.toString.call(arr) !== "[object Array]"){
+    throw new Error()
   }
- return array;
-};
+  const test = [...arr];
+  const res = test.reduce((acc, elem, index, array) => {
+    switch (elem) {
+        case "--discard-next":
+          if (index !== acc.length-1){ acc[index+1] = 'q`'};
+          acc[index] = 'q`';
+          break;
+          case "--discard-prev":
+            if (index !== 0){ acc[index-1] = 'q`'};
+            acc[index] = 'q`';
+          break;
+          case "--double-next":
+            if (index !== acc.length-1){ acc[index] = acc[index+1]}
+          else{acc[index]='q`'}
+          break;
+          case "--double-prev":
+            if (index !== 0){ acc[index] = acc[index-1]}
+            else{acc[index]='q`'}
+          break;
+          default:
+            break;
+      }
+      return acc;
+  },test).filter(el=>el!=='q`')
+  return res
+} 
+catch (error) {
+  throw error
+}
+}
--- src/what-season.js
+++ src/what-season.js
@@ -2,11 +2,24 @@ const CustomError = require("../extensions/custom-error");
 
 module.exports = function getSeason(date) {
   // throw new CustomError('Not implemented');
-  if (date == null) return 'Unable to determine the time of year!';
-  if (!(date instanceof Date)) throw new Error();
-  if (!date) return Error;
-  let month = date.getMonth();
+  // if (date == null) return 'Unable to determine the time of year!';
+  // if (!(date instanceof Date)) throw new Error;
+  // if (!date) return Error;
+  // console.log(date)
+  // let month = date.getMonth();
   let timeOfTheYear = ['winter', 'winter', 'spring', 'spring', 'spring', 'summer', 'summer', 'summer', 'autumn', 'autumn', 'autumn', 'winter'];
   
-  return timeOfTheYear[month]; 
+  try {
+    if (Object.prototype.toString.call(date) === "[object Date]"){
+      return timeOfTheYear[date.getMonth()];
+    } else {
+      throw new Error('Unable to determine the time of year!')
+    }
+  }
+  catch(error){
+    if (typeof date == 'undefined'){
+      return error.message
+    }
+    throw error
+  }
 };
